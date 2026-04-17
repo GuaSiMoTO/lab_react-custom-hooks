fba857f849cb44ee13424e3a6b738e8f6d0451b9
GuaSiMoTO <gemelogemelo@hotmail.com>
2026-04-17 16:44:16 +0200

    Bonus terminados

---
+++ lab-react-custom-hooks/src/hooks/useAccordion.js
@@ -0,0 +1,27 @@
+// Iteration 5 (BONUS)
+import { useState } from "react";
+
+const useAccordion = (initialValue = false) => {
+  // Definimos si el acordeón está abierto o cerrado.
+  // Por defecto es 'false' (cerrado).
+  const [isOpen, setIsOpen] = useState(initialValue);
+  
+  // Función para alternar el estado del acordeón
+  const toggle = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+
+  // controles de open y close
+  const opnen = () => setIsOpen(true);
+  const close = () => setIsOpen(false);
+
+  return { isOpen, toggle, open, close};
+
+
+};
+
+export default useAccordion;
+
+// yo no use la función de REACT useCallback.. Se usa para evitar que se renderize innnecesariamente
+// se añaden en las const open = useCallback(); la función arrow
--- lab-react-custom-hooks/src/hooks/useFetch.js
+++ lab-react-custom-hooks/src/hooks/useFetch.js
@@ -10,17 +10,31 @@ const useFetch = (url) => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
+
+    // Iteration 4 (BONUS): creamos cancelar la solicitud HTTP
+    const controller = new AbortController();
+
     // Aqui es donde llamas a la API con Axios
     const fetchData = async () => {
     
     setLoading(true); // Empezamos a cargar, antes del try para que se vea el loading aunque la URL sea rápida
     
     try {
-        const response = await axios.get(url); // llama al servidor
+      // añadimos el signal del controller a la solicitud para poder cancelarla si es necesario
+        const response = await axios.get(url,
+          {signal: controller.signal} // con signal le pasamos abortcontroller, antes creada
+        );
+
         setData(response.data); // Axios guarda la info en .data
         setError(null); // limpiamos el error si la llamada fue exitosa
-      } catch (err) {
-        setError(err); // capturamos el error
+      } catch (e) {
+
+        // manejamos si el error es por cancelación
+        if (axios.isCancel(e)) {
+           console.log( "Petición Cancelada: " , err.message);
+           return
+        }
+        setError(e); // capturamos el error
         setData(null); // limpiamos los datos si hubo un error
       } finally {
         setLoading(false); // Terminamos de cargar pase lo q sea
@@ -28,6 +42,10 @@ const useFetch = (url) => {
     };
 
     fetchData(); // ejecutamos la función
+
+    // Devolver función de limpieza. A esto se le llama "cleanup function" y se ejecuta cuando el componente se desmonta o antes de que el efecto se vuelva a ejecutar (si la URL cambia). Esto es importante para evitar actualizar el estado de un componente que ya no está montado, lo que puede causar errores.
+    return () => { controller.abort(); }; // Esto cancela la solicitud si el componente se desmonta o si la URL cambia antes de que la solicitud termine
+  
   }, [url]); // Importante: si la URL cambia, el efecto se vuelve a ejecutar
 
   // El hook SIEMPRE debe devolver algo para que el componente lo use
